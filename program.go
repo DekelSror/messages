ef01d8089b73aaccfc38306237cbed8e8eec79ed
DekelSror <srordekel@gmail.com>
2021-10-22 14:09:49 +0300

    messages working!

--- program.go
+++ program.go
@@ -5,118 +5,31 @@ import (
     "log"
     "net/http"
     "encoding/json"
+
 )
 
-type UserId int
-
-type User struct {
-    name string
-    id UserId
-}
-
 type Message struct {
-    from UserId
-    to UserId
-    content string
+    From string `json:"from"`
+    To string	`json:to`
+    Content string `json:content`
 }
-
-var users []User = []User{ {name: "fekel", id: 1}, {name: "odan", id: 0} }
-var nextUserId = 2
-
-var messages map[string][]string
-
-func PostUser(w http.ResponseWriter, r *http.Request) {
-    q := r.URL.Query()
-    userName := q["userName"][0]
-
-    if userName == "" {
-        http.Error(w, "shoot", http.StatusBadRequest)
-        return
-    }
-
-    for i := 0; i < len(users); i++ {
-        if users[i].name == userName {
-            http.Error(w, "username exists", http.StatusBadRequest)
-            return
-        }
-
-    }
-
-    users = append(users, User{name: userName, id: UserId(nextUserId)})
-    nextUserId++
-    fmt.Fprintf(w, "user %s added!", userName)
-    http.StatusText(http.StatusOK)
-}
-
-func StartSession(w http.ResponseWriter, r *http.Request) {
-
-
-
-}
-
-func ConversationId (from string, to string) string {
-    if from >= to {
-        return to + from
-    } else {
-        return from + to
-    }
-}
-
-
 
 func SendMessage(w http.ResponseWriter, r *http.Request) {    
-    var m Message
-    error := json.NewDecoder(r.Body).Decode(&m)
-
-    if error != nil {
-        fmt.Fprintf(w, error.Error())
-        http.Error(w, "bad message", http.StatusBadRequest)
-        return
-    }
-
-    q := r.URL.Query()
-    from := q["fromUser"][0]
-    to := q["toUser"][0]
+	fmt.Println("send message called")
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+    w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
     
-    conversationId := ConversationId(from, to)    
-
-    if messages[conversationId] == nil {
-        messages[conversationId] = []string{m.content}
-    } else {
-        messages[conversationId] = append(messages[conversationId], m.content)
-    }
+	var m Message
+	err := json.NewDecoder(r.Body).Decode(&m)
+	
+	if err != nil {}
+	
+	fmt.Fprintf(w, "%+v", m)
 }
 
-
-func GetHistory (w http.ResponseWriter, r *http.Request) {
-    q := r.URL.Query()
-    from := q["fromUser"][0]
-    to := q["toUser"][0]
-    
-    conversationId := ConversationId(from, to)
-
-    mstr, err := json.Marshal(messages[conversationId])
-
-    if err != nil {return}
-
-    fmt.Fprintf(w, "%s", mstr)    
-}
 
 func main() {
-    http.HandleFunc("/addUser", PostUser)
-    
-    http.HandleFunc("/listUsers", func(w http.ResponseWriter, r *http.Request) {
-        var res string
-        
-        for i := 0; i < len(users); i++ {
-            res += users[i].name + " "
-        }
-
-        fmt.Fprintf(w, "%s", res)
-    })
-
-
-    http.HandleFunc("/viewHistory", GetHistory)
-
+	fmt.Println("hi!")
+    http.HandleFunc("/sendMessage", SendMessage)
     log.Fatal(http.ListenAndServe(":8080", nil)) // Serve & Protect
 }
