3b0167004fbb411fe0393ee09b72e122e75fa684
DekelSror <srordekel@gmail.com>
2021-11-17 19:14:07 +0200

    started enc

--- api.go
+++ api.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 type Message struct {
@@ -12,8 +14,36 @@ type Message struct {
 	Content string `json:"content"`
 }
 
+func MockDecode(encrypted_string string) string {
+	var mask uint64
+	mask = 0x123456789abcdef0
+	var result string
+
+	for i := 0; i < len(encrypted_string)/8; i++ {
+		piece, err := strconv.ParseUint(encrypted_string[i*8:i*8+8], 16, 64)
+
+		if err != nil {
+
+		}
+
+		decrypted_piece := piece ^ mask
+		result += strconv.FormatUint(decrypted_piece, 16)
+
+	}
+
+	fmt.Print(fmt.Sprintf("result %s \n", result))
+
+	return result
+}
+
 func PostMessage(w http.ResponseWriter, r *http.Request, server Server) {
 	var m Message
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r.Body)
+	encrypted_string := buf.String()
+	MockDecode(encrypted_string)
+
 	err := json.NewDecoder(r.Body).Decode(&m)
 
 	if err != nil {
--- program.go
+++ program.go
@@ -1,13 +1,15 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func main() {
+
 	s := MakeServer()
 
 	s.Teach("/sendMessage", PostMessage)
 	s.Teach("/history", GetMessages)
 
 	http.ListenAndServe(":8080", s)
-
 }
